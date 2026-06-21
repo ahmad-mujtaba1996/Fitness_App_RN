00171d5ed90e60865c01967e8e29ab99bc0e05bf
Syed Ahmad Mujtaba <amujtaba1996@gmail.com>
2026-06-21 21:18:34 +0800

    FitnessApp: Main Home Screen, Parallex scrolling added.

--- elite-fitness-app/app/home.jsx
+++ elite-fitness-app/app/home.jsx
@@ -1,13 +1,15 @@
+import { Ionicons } from "@react-native-vector-icons/ionicons";
 import { Image, Text, View } from "react-native";
 import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 import { SafeAreaView } from "react-native-safe-area-context";
+import ImageSlider from "../components/image_slider";
 
 export default function Home() {
   return (
-    <SafeAreaView className="flex-1 bg-white flex space-y-5" edges={["top"]}>
+    <SafeAreaView className="flex-1 bg-white flex gap-y-5" edges={["top"]}>
       {/* punch line and avatar */}
       <View className="flex-row justify-between items-center mx-5">
-        <View className="space-y-2">
+        <View className="gap-y-2">
           <Text
             style={{ fontSize: hp(4.5) }}
             className="font-bold tracking-wider text-neutral-700"
@@ -21,14 +23,23 @@ export default function Home() {
             WORKOUT
           </Text>
         </View>
-        <View className="flex justify-center">
+        <View className="flex justify-center items-center gap-y-2">
           <Image
             source={require("../assets/images/avatar.png")}
             style={{ height: hp(6), width: hp(6) }}
             className="rounded-full "
           />
-          <View className="bg-neutral-200 rounded-full flex justify-center items-center"></View>
+          <View
+            className="bg-neutral-200 rounded-full flex justify-center items-center border-[3px] border-neutral-300"
+            style={{ width: hp(5.5), height: hp(5.5) }}
+          >
+            <Ionicons name="notifications" size={hp(3)} color="gray" />
+          </View>
         </View>
+      </View>
+      {/* Image Slider */}
+      <View>
+        <ImageSlider />
       </View>
     </SafeAreaView>
   );
